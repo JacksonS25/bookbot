029c333fe1fa92c8d90a2ce280311402807172cd
holyrobots <jks.sheesley@gmail.com>
2025-05-28 22:12:47 -0500

    E: Add stats.py, Refactor

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+from stats import get_num_words
+
 def get_book_text(book_location):
     with open(book_location) as book:
         return book.read()
-
-def get_num_words(book_text):
-    word_list = book_text.split()
-    return len(word_list)
 
 def main():
     book = get_book_text("./books/frankenstein.txt")
