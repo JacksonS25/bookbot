a4ef6d9f9b5e2b6ec5d7da3f71768784f6777bf5
holyrobots <jks.sheesley@gmail.com>
2025-05-28 22:07:44 -0500

    D: Print Number of Words

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ def get_book_text(book_location):
     with open(book_location) as book:
         return book.read()
 
+def get_num_words(book_text):
+    word_list = book_text.split()
+    return len(word_list)
+
 def main():
-    print(get_book_text("./books/frankenstein.txt"))
+    book = get_book_text("./books/frankenstein.txt")
+    num_words = get_num_words(book)
+    print(f"{num_words} words found in the document")
 
 main()
