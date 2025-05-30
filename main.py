679d26540aa7f792e620ecae5425e88bd06fabe2
holyrobots <jks.sheesley@gmail.com>
2025-05-29 22:26:58 -0500

    F: Finish program with character count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, count_characters
 
 def get_book_text(book_location):
     with open(book_location) as book:
@@ -8,5 +8,7 @@ def main():
     book = get_book_text("./books/frankenstein.txt")
     num_words = get_num_words(book)
     print(f"{num_words} words found in the document")
+    character_count = count_characters(book)
+    print(character_count)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def get_num_words(book_text):
     word_list = book_text.split()
     return len(word_list)
+
+def count_characters(book_text):
+    character_count = {}
+    for word in book_text:
+        letters = word.split()
+        for letter in letters:
+            if letter.lower() in character_count:
+                character_count[letter.lower()] += 1
+            else:
+                character_count[letter.lower()] = 1
+    return character_count
